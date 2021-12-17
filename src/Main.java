ef12176888dc9c59062bf7553915759a4b397555
Roksana <gdx148245@student.wsb.gda.pl>
2021-12-18 00:12:42 +0100

    Zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+System.out.print("Hello world ");
+System.out.println("witaj swiecie");
+System.out.print("życzę" + " " + "wam " + "miłej nauki!");
+System.out.printf(" A %s zaczynamy z %d przykładem. \n", "więc", 1);
+System.out.print("to jest jakiś");
+System.out.print(" tekst");
+System.out.print(" nie za długi.");
     }
 }
